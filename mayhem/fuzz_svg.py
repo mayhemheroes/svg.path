9a86fccf51098259e00be0f4f52c6dbf6b23761c
Bailey Capuano <bcapuano@asu.edu>
2022-12-29 21:45:22 -0500

    Initial MayhemHeroes Integration

---
+++ mayhem/fuzz_svg.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import atheris
+import sys
+import fuzz_helpers
+import random
+
+with atheris.instrument_imports():
+    from svg.path import parse_path
+
+from svg.path.parser import InvalidPathError
+
+def TestOneInput(data):
+    fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
+    try:
+        parse_path(fdp.ConsumeRemainingString())
+    except (InvalidPathError):
+        return -1
+    except (TypeError, ValueError, AttributeError, IndexError):
+        if random.random() > 0.99:
+            raise
+        return -1
+def main():
+    atheris.Setup(sys.argv, TestOneInput)
+    atheris.Fuzz()
+
+
+if __name__ == "__main__":
+    main()
